00e8bb34905774f018d9bb18c75e4b24195c8fb7
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-24 10:32:02 +0200

    Create basic list with clothes.

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/http'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/http', 'rxjs/add/operator/map'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -11,7 +11,7 @@ System.register(['angular2/core', 'angular2/http'], function(exports_1, context_
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1, http_1;
-    var AppComponent;
+    var AppComponent, CLOTHES;
     return {
         setters:[
             function (core_1_1) {
@@ -19,16 +19,19 @@ System.register(['angular2/core', 'angular2/http'], function(exports_1, context_
             },
             function (http_1_1) {
                 http_1 = http_1_1;
-            }],
+            },
+            function (_1) {}],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent(http) {
-                    var _this = this;
+                function AppComponent() {
                     this.title = 'ClothesAi';
-                    http.get('http://api-clothesai.herokuapp.com/v1/clothes')
-                        .map(function (res) { return res.json(); })
-                        .subscribe(function (clothes) { return _this.clothes = clothes; });
+                    this.clothes = CLOTHES;
                 }
+                //constructor(http:Http) {
+                //    http.get('http://api.clothesai.app/v1/clothes')
+                //        .map(res => res.json())
+                //        .subscribe(clothes => this.clothes = clothes);
+                //}
                 AppComponent.prototype.onSelect = function (cloth) {
                     this.selectedCloth = cloth;
                 };
@@ -36,15 +39,28 @@ System.register(['angular2/core', 'angular2/http'], function(exports_1, context_
                     core_1.Component({
                         selector: 'my-app',
                         viewProviders: [http_1.HTTP_PROVIDERS],
-                        template: "\n    <h1>{{title}}</h1>\n    <h2>Clothes</h2>\n    <ul class=\"clothes\">\n        <li *ngFor=\"#cloth of clothes\"\n            [class.selected]=\"cloth === selectedCloth\"\n            (click)=\"onSelect(cloth\">\n            <span class=\"badge\">{{cloth.name}}</span> {{\n        </li>\n    </ul>\n    <div><label>Description: </label>{{cloth.description}}</div>\n    <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"cloth.name\" placeholder=\"name\">\n    </div>\n    "
+                        template: "\n        <h1>{{title}}</h1>\n        <h2>Clothes</h2>\n        <ul class=\"clothes\">\n            <li *ngFor=\"#cloth of clothes\"\n                [class.selected]=\"cloth === selectedCloth\"\n                (click)=\"onSelect(cloth)\">\n                <span class=\"badge\">{{cloth.slug}}</span> {{cloth.name}}\n            </li>\n        </ul>\n        <div *ngIf=\"selectedCloth\">\n            <h2>{{selectedCloth.name}} details</h2>\n            <div><label>slug: </label>{{selectedCloth.slug}}</div>\n            <div>\n                <label>name: </label>\n                <input [(ngModel)]=\"selectedCloth.name\" placeholder=\"name\"/>\n            </div>\n        </div>\n    ",
+                        styles: ["\n .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .clothes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 10em;\n    }\n    .clothes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .clothes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .clothes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .clothes .text {\n      position: relative;\n      top: -3px;\n    }\n    .clothes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
                     }), 
-                    __metadata('design:paramtypes', [http_1.Http])
+                    __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            //console.log('clothes: ' + this.clothes);
+            CLOTHES = [
+                { "slug": "11", "name": "Mr. Nice", "description": "description1" },
+                { "slug": "12", "name": "Narco", "description": "description2" },
+                { "slug": "13", "name": "Bombasto", "description": "description3" },
+                { "slug": "14", "name": "Celeritas", "description": "description4" },
+                { "slug": "15", "name": "Magneta", "description": "description5" },
+                { "slug": "16", "name": "RubberMan", "description": "description6" },
+                { "slug": "17", "name": "Dynama", "description": "description7" },
+                { "slug": "18", "name": "Dr IQ", "description": "description8" },
+                { "slug": "19", "name": "Magma", "description": "description19" },
+                { "slug": "20", "name": "Tornado", "description": "description10" },
+            ];
         }
     }
 });
-//var CLOTHES: Cloth[] = http.get('people.json').observer({next: (value) => this.people = value});
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,13 @@
  * @since 23/03/16
  */
 import {Component} from 'angular2/core';
+import {Http} from "angular2/http";
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/add/operator/map';
 
 interface Cloth {
     name: string;
+    slug: string;
     description: string;
 }
 
@@ -14,36 +17,100 @@ interface Cloth {
     selector: 'my-app',
     viewProviders: [HTTP_PROVIDERS],
     template: `
-    <h1>{{title}}</h1>
-    <h2>Clothes</h2>
-    <ul class="clothes">
-        <li *ngFor="#cloth of clothes"
-            [class.selected]="cloth === selectedCloth"
-            (click)="onSelect(cloth">
-            <span class="badge">{{cloth.name}}</span> {{
-        </li>
-    </ul>
-    <div><label>Description: </label>{{cloth.description}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="cloth.name" placeholder="name">
-    </div>
-    `
+        <h1>{{title}}</h1>
+        <h2>Clothes</h2>
+        <ul class="clothes">
+            <li *ngFor="#cloth of clothes"
+                [class.selected]="cloth === selectedCloth"
+                (click)="onSelect(cloth)">
+                <span class="badge">{{cloth.slug}}</span> {{cloth.name}}
+            </li>
+        </ul>
+        <div *ngIf="selectedCloth">
+            <h2>{{selectedCloth.name}} details</h2>
+            <div><label>slug: </label>{{selectedCloth.slug}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedCloth.name" placeholder="name"/>
+            </div>
+        </div>
+    `,
+    styles: [`
+ .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .clothes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 10em;
+    }
+    .clothes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .clothes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .clothes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .clothes .text {
+      position: relative;
+      top: -3px;
+    }
+    .clothes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 export class AppComponent {
     public title = 'ClothesAi';
-    clothes;
+    clothes = CLOTHES;
     selectedCloth:Cloth;
 
-    constructor(http:Http) {
-        http.get('http://api-clothesai.herokuapp.com/v1/clothes')
-            .map(res => res.json())
-            .subscribe(clothes => this.clothes = clothes);
-    }
+    //constructor(http:Http) {
+    //    http.get('http://api.clothesai.app/v1/clothes')
+    //        .map(res => res.json())
+    //        .subscribe(clothes => this.clothes = clothes);
+    //}
 
     onSelect(cloth:Cloth) {
         this.selectedCloth = cloth;
     }
 }
+//console.log('clothes: ' + this.clothes);
 
-//var CLOTHES: Cloth[] = http.get('people.json').observer({next: (value) => this.people = value});
+var CLOTHES:Cloth[] = [
+    {"slug": "11", "name": "Mr. Nice", "description": "description1"},
+    {"slug": "12", "name": "Narco", "description": "description2"},
+    {"slug": "13", "name": "Bombasto", "description": "description3"},
+    {"slug": "14", "name": "Celeritas", "description": "description4"},
+    {"slug": "15", "name": "Magneta", "description": "description5"},
+    {"slug": "16", "name": "RubberMan", "description": "description6"},
+    {"slug": "17", "name": "Dynama", "description": "description7"},
+    {"slug": "18", "name": "Dr IQ", "description": "description8"},
+    {"slug": "19", "name": "Magma", "description": "description19"},
+    {"slug": "20", "name": "Tornado", "description": "description10"},
+];
