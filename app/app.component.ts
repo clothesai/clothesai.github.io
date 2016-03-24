0342466101c7ca992f88f7ce1a835bfb208b269a
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-24 07:16:22 +0200

    Add basic edit search option two-way binding.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,27 @@
  */
 import {Component} from 'angular2/core';
 
+interface Cloth {
+    slug: string;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>Search</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{cloth.name}}</h2>
+    <div><label>slug: </label>{{cloth.slug}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="cloth.name" placeholder="name">
+    </div>
+    `
 })
-export class AppComponent{}
+export class AppComponent {
+    public title = 'ClothesAi';
+    public cloth:Cloth = {
+        slug: 'red-shirt',
+        name: 'Red Shirt'
+    }
+}
