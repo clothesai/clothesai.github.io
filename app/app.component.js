caadd122c37584df52ab6dda42ec96851fbde236
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-24 07:35:30 +0200

    Make preparations for consuming /api/v1/clothes Api.

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/http'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,28 +10,35 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, http_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
+                function AppComponent(http) {
+                    var _this = this;
                     this.title = 'ClothesAi';
-                    this.cloth = {
-                        slug: 'red-shirt',
-                        name: 'Red Shirt'
-                    };
+                    http.get('http://api-clothesai.herokuapp.com/v1/clothes')
+                        .map(function (res) { return res.json(); })
+                        .subscribe(function (clothes) { return _this.clothes = clothes; });
                 }
+                AppComponent.prototype.onSelect = function (cloth) {
+                    this.selectedCloth = cloth;
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <h2>{{cloth.name}}</h2>\n    <div><label>slug: </label>{{cloth.slug}}</div>\n    <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"cloth.name\" placeholder=\"name\">\n    </div>\n    "
+                        viewProviders: [http_1.HTTP_PROVIDERS],
+                        template: "\n    <h1>{{title}}</h1>\n    <h2>Clothes</h2>\n    <ul class=\"clothes\">\n        <li *ngFor=\"#cloth of clothes\"\n            [class.selected]=\"cloth === selectedCloth\"\n            (click)=\"onSelect(cloth\">\n            <span class=\"badge\">{{cloth.name}}</span> {{\n        </li>\n    </ul>\n    <div><label>Description: </label>{{cloth.description}}</div>\n    <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"cloth.name\" placeholder=\"name\">\n    </div>\n    "
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [http_1.Http])
                 ], AppComponent);
                 return AppComponent;
             }());
@@ -39,4 +46,5 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         }
     }
 });
+//var CLOTHES: Cloth[] = http.get('people.json').observer({next: (value) => this.people = value});
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,18 +3,27 @@
  * @since 23/03/16
  */
 import {Component} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
 
 interface Cloth {
-    slug: string;
     name: string;
+    description: string;
 }
 
 @Component({
     selector: 'my-app',
+    viewProviders: [HTTP_PROVIDERS],
     template: `
     <h1>{{title}}</h1>
-    <h2>{{cloth.name}}</h2>
-    <div><label>slug: </label>{{cloth.slug}}</div>
+    <h2>Clothes</h2>
+    <ul class="clothes">
+        <li *ngFor="#cloth of clothes"
+            [class.selected]="cloth === selectedCloth"
+            (click)="onSelect(cloth">
+            <span class="badge">{{cloth.name}}</span> {{
+        </li>
+    </ul>
+    <div><label>Description: </label>{{cloth.description}}</div>
     <div>
         <label>name: </label>
         <input [(ngModel)]="cloth.name" placeholder="name">
@@ -23,8 +32,18 @@ interface Cloth {
 })
 export class AppComponent {
     public title = 'ClothesAi';
-    public cloth:Cloth = {
-        slug: 'red-shirt',
-        name: 'Red Shirt'
+    clothes;
+    selectedCloth:Cloth;
+
+    constructor(http:Http) {
+        http.get('http://api-clothesai.herokuapp.com/v1/clothes')
+            .map(res => res.json())
+            .subscribe(clothes => this.clothes = clothes);
+    }
+
+    onSelect(cloth:Cloth) {
+        this.selectedCloth = cloth;
     }
 }
+
+//var CLOTHES: Cloth[] = http.get('people.json').observer({next: (value) => this.people = value});
