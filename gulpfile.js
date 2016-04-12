9cc0cc6fadabb33df985bf674f7cdc586a625b72
Rizart Dokollari <r.dokollari@gmail.com>
2016-04-12 16:07:52 +0000

    Set up gulp. Resolve #11

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,37 @@
-var gulp = require('gulp');
-var stripDebug = require('gulp-strip-debug');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    stripDebug = require('gulp-strip-debug'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    changed = require('gulp-changed'),
+    cache = require('gulp-cached'),
+    remember = require('gulp-remember');
 
 gulp.task('default', function () {
 });
 
 gulp.task('assets', function () {
-    gulp.src([
-            'node_modules/es6-shim/es6-sham.min.js',
-            'node_modules/systemjs/dist/system-polyfills.js',
-            'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
-            'node_modules/angular2/bundles/angular2-polyfills.js',
-            'node_modules/systemjs/dist/system.js',
-            'node_modules/typescript/lib/typescript.js',
-            'node_modules/rxjs/Rx.js',
-            'node_modules/angular2/bundles/angular2.dev.js',
-            'node_modules/angular2/bundles/router.dev.js',
-            'node_modules/angular2/bundles/http.dev.js'
-        ])
-        .pipe(concat('public/master.min.js'))
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(gulp.dest('./build/js/'));
+    const DEST = './master.js';
+    const SRC = [
+        './node_modules/es6-shim/es6-sham.js',
+        './node_modules/systemjs/dist/system-polyfills.js',
+        './node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
+        './node_modules/angular2/bundles/angular2-polyfills.js',
+        './node_modules/systemjs/dist/system.js',
+        './node_modules/typescript/lib/typescript.js',
+        './node_modules/rxjs/Rx.js',
+        './node_modules/angular2/bundles/angular2.dev.js',
+        './node_modules/angular2/bundles/router.dev.js',
+        './node_modules/angular2/bundles/http.dev.js'
+    ];
+
+    return gulp.src(SRC)
+        // .pipe(cache('scripts'))
+        // .pipe(stripDebug())
+        // .pipe(cache('scripts'))
+        // .pipe(uglify())
+        .pipe(concat('master.js'))
+        .pipe(gulp.dest('./build/js'))
+        .pipe(rename('master.min.js'))
+        .pipe(gulp.dest('./build/js'));
 });
